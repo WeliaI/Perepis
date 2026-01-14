f657cd9f95df8e4161edd2694687fbb523c6ecc7
WeliaI <106620836+WeliaI@users.noreply.github.com>
2026-01-14 18:03:04 +0600

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ for i in L:
 diap1 = int(input('\nВведите первое число диапозона: '))
 diap2 = int(input('Введите второе число диапозона: '))
 print()
+
 for i in L:
     people = i.split()
     if diap1 < int(people[-1].split('.')[-1]) < diap2:
